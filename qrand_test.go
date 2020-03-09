85244c1da2c34fec3697b36ff0ffb4e3a4d05e79
John Arundel <john@bitfieldconsulting.com>
2020-03-09 18:23:21 +0000

    Update test data

--- qrand_test.go
+++ qrand_test.go
@@ -13,35 +13,11 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-// func TestReadCannedData(t *testing.T) {
-// 	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		data, err := os.Open("testdata/response.json")
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-// 		w.WriteHeader(http.StatusOK)
-// 		defer data.Close()
-// 		io.Copy(w, data)
-// 	}))
-// 	defer ts.Close()
-// 	qrand.HTTPClient = ts.Client()
-// 	qrand.URL = ts.URL
-// 	const numBytes = 8
-// 	got := make([]byte, numBytes)
-// 	n, err := qrand.Read(got)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	if n != numBytes {
-// 		t.Errorf("want %d bytes, got %d", numBytes, n)
-// 	}
-// }
-
 func TestBytes(t *testing.T) {
 	called := false
 	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		called = true
-		wantURL := "/API/jsonI.php?length=1&type=uint8&size=12"
+		wantURL := "/API/jsonI.php?length=64&type=uint8"
 		if !cmp.Equal(wantURL, r.URL.String()) {
 			t.Error(cmp.Diff(wantURL, r.URL.String()))
 		}
@@ -56,13 +32,13 @@ func TestBytes(t *testing.T) {
 	defer ts.Close()
 	qrand.HTTPClient = ts.Client()
 	qrand.URL = ts.URL
-	var got = make([]byte, 12)
+	var got = make([]byte, 64)
 	bytesRead, err := qrand.Read(got)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if bytesRead != 12 {
-		t.Errorf("want 12 bytes read, got %d", bytesRead)
+	if bytesRead != 64 {
+		t.Errorf("want 64 bytes read, got %d", bytesRead)
 	}
 	if !called {
 		t.Error("want API call, but got none")
@@ -74,7 +50,7 @@ func TestUnmarshalJSON(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	want := qrand.APIResponse{[]byte("5258aa2852307702")}
+	want := qrand.APIResponse{[]byte{63, 25, 21, 239, 178, 131, 81, 166, 228, 3, 236, 255, 228, 216, 52, 195, 139, 187, 223, 161, 45, 5, 175, 173, 47, 57, 24, 212, 196, 34, 195, 132, 224, 211, 212, 6, 85, 135, 159, 57, 155, 213, 23, 33, 239, 85, 255, 76, 163, 51, 15, 251, 45, 216, 100, 123, 171, 8, 209, 92, 220, 207, 73, 172}}
 	got := qrand.APIResponse{}
 	err = json.Unmarshal(jData, &got)
 	if err != nil {
